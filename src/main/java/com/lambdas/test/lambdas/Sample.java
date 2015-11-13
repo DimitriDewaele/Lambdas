c9d854dc8f2a35a3b32404eb9f8a0c1e71e6e3a3
Dimitri Dewaele <dimitri.dewaele@mips.be>
2015-11-13 12:13:12 +0100

    playing with threads

--- src/main/java/com/lambdas/test/lambdas/Sample.java
+++ src/main/java/com/lambdas/test/lambdas/Sample.java
@@ -3,28 +3,37 @@ package com.lambdas.test.lambdas;
 import java.util.*;
 
 public class Sample {
-  public static void main(String[] args) {
-      
-    //Anonymous inber function has 4 things:
-    //1. name - anonymous
-          //2. parameter list
-          //3. body
-    //4. return type - inferred
-      
-    // NO Lamba's
-    //    Thread th = new Thread(new Runnable() {
-    //      public void run() {
-    //        System.out.println("In another thread");
-    //      }
-    //    });
-    //    th.start();
-   
 
+    private static int count = 1;
 
-    // WITH Lambda
-    Thread th = new Thread(() -> System.out.println("In another thread"));
-    th.start();
-    
-    System.out.println("In main");
-  }
+    public static void main(String[] args) {
+
+        Thread th;
+        
+        //Anonymous inber function has 4 things:
+            //1. name - anonymous
+            //2. parameter list
+            //3. body
+            //4. return type - inferred
+
+        // NO Lamba's
+        th = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("In a thread without Lambda");
+            }
+        });
+        th.start();
+
+        // WITH Lambda
+        th = new Thread(() -> System.out.println("In thread " + count++));
+        th.start();
+
+        th = new Thread(() -> System.out.println("In thread " + count++));
+        th.start();
+
+        th = new Thread(() -> System.out.println("In thread " + count++));
+        th.start();
+
+        System.out.println("In main");
+    }
 }
